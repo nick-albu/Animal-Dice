5ad9cb30478691b09b3596ebc0d9c0dbef6691bb
nick-albu <nicolasmalbu@gmail.com>
2022-09-23 12:34:26 -0400

    Revert "roughed out app structure"
    This reverts commit 4ad0312d2ccce6b8622e64487fa68c311cd87c92.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './pages/Main';
+import Nav from './pages/Filter';
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Filter.js
@@ -0,0 +1,11 @@
+import './Filter.scss'
+
+function Nav () {
+    return (
+        <div className='filter'>
+            <div></div>
+        </div>
+    )
+}
+
+export default Nav;
